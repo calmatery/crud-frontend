024a45d0fe9275cbb1ec84d99c6312149a3517d5
wuxinming <wuxinming@utry.cn>
2020-01-30 22:42:00 +0800

    更新

--- wxm-crud/DesignerPanel.vue
+++ wxm-crud/DesignerPanel.vue
@@ -2,6 +2,7 @@
     <div class="designerPanel">
         <a-form>
             <draggable :style="'height:500px'" group="designer" handle=".component-drag"
+                       ghostClass="dragging"
                    v-model="itemList" @add="itemAdd">
                 <i-designer-item v-for="item in itemList" :key="item.key"
                                  :value="item" :selected.sync="selected"></i-designer-item>
@@ -49,19 +50,17 @@
     .wxm-crud-designer .designerPanel{
         padding: 0px 4px;
     }
-    .wxm-crud-designer .designerPanel .component-icon, .wxm-crud-designer .designerPanel .component-icon:hover {
+    .wxm-crud-designer .designerPanel .component-icon,
+    .wxm-crud-designer .designerPanel .dragging,
+    .wxm-crud-designer .designerPanel .component-icon:hover {
         border: 2px solid red;
         width: 100%;
         height: 0;
         margin: 0;
+        outline: none;
+        overflow: hidden;
     }
-
-    .wxm-crud-designer .designerPanel .component-icon > i {
-        display: none;
-
-    }
-
-    .wxm-crud-designer .designerPanel .component-icon > span {
-        display: none;
+    .wxm-crud-designer .designerPanel .dragging{
+        float: left;
     }
 </style>
